201671e0d53059ce4b9be0c2c07e9e325f839491
hamidpahlavan <hamid.a.pahlavan@gmail.com>
2018-11-07 22:13:30 -0800

    committing code that matches census and ycom data at county level

--- ycom_census_county_match.py
+++ ycom_census_county_match.py
@@ -9,7 +9,6 @@ import pandas as pd
 import numpy as np
 
 # loading datasets
-
 ycom = pd.read_csv('YCOM_2018_Data.csv', encoding='latin-1')
 census = pd.read_csv('us-census-demographic-data/acs2015_county_data.csv')
 
@@ -22,19 +21,16 @@ ycom_county = ycom_county.reset_index(drop=True)
 
 # Separating the counties and states from 'GeoType' column and add those as
 # separated columns to the 'ycom_county' dataframe.
-
 county_state_sep = pd.DataFrame(ycom_county.GeoName.str.split(',').tolist())
 ycom_county['State'] = county_state_sep[1]
 ycom_county['County'] = county_state_sep[0]
 
 # Dropping 'county' and 'Parish' words and the last whitespace
 # from the counties.
-
 ycom_county['County'] = ycom_county['County'].str.replace('County', '')
 ycom_county['County'] = ycom_county['County'].str.replace('Parish', '')
 ycom_county['County'] = ycom_county['County'].str.strip()
 
 # test if the counties are the same and in order in two datasets.
-
 if np.all(ycom_county['County'] == census['County']):
     print('Yaaaaay!')
