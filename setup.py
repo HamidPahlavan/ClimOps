47df4cc73aeb58b5eb032c7da6b1021cd3c4c224
Yakelyn R. Jauregui <yaki@D-10-19-222-109.dhcp4.washington.edu>
2018-12-05 14:49:28 -0800

    ready but missing continuos integration

--- setup.py
+++ setup.py
@@ -11,28 +11,27 @@ with open("README", 'r') as f:
     long_description = f.read()
 
 setup(
-    name='climaps',  # Required
+    name='climops',  # Required
     version='0.1.0',  # Required
     description='Python interface to get statistical relationships from Yale Climate opinion maps and Census',  # Optional
     # Often, this is the same as your README, so you can just read it in from
     # that file directly (as we have already done above)
-    url='https://github.com/https://github.com/HamidPahlavan/project',  # Optional but need to change the name
+    url='https://github.com/https://github.com/HamidPahlavan/ClimOps',  
     license = 'MIT'
     author='Robin Clancy, Rebeca de Buen, Hamid Pahlavan and Yakelyn R. Jauregui',  # Optional
     author_email='rclancy@uw.edu',  # Optional
-    packages =['climaps'],
+    packages =['climops'],
     keywords='Yale Climate opinion maps, Census',  # Optional
     # Similar to `install_requires` above, these must be valid existing
     # projects.
     package_data=['data/*']
-    scripts=['add here','more']
     extras_require={  # Optional
         'dev': ['check-manifest'],
         'test': ['coverage'],
     },
 
     project_urls={  # Optional
-        'Bug Reports': 'https://github.com/https://github.com/HamidPahlavan/project/issues',
-        'Source': 'https://github.com/https://github.com/HamidPahlavan/project/',
+        'Bug Reports': 'https://github.com/https://github.com/HamidPahlavan/ClimOps/issues',
+        'Source': 'https://github.com/https://github.com/HamidPahlavan/ClimOps/',
     },
 )
